36ffe656d32d31f5034596afcd227c9828668e1c
KeremVona <keremvona@gmail.com>
2025-05-18 15:46:23 +0300

    u

--- nursing services company website/vite.config.js
+++ nursing services company website/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/nursing-services-company-website/",
+  base: "/nursing-services-company-website",
 });
